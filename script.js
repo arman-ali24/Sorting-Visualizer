05bc09131a9a52381dd9b10e54aee151d599e014
Arman Ali <armanali0178614@gmail.com>
2025-12-07 14:26:32 +0530

    some chnages

--- script.js
+++ script.js
@@ -167,9 +167,9 @@ $(document).ready(function () {
             await insertionSort();
         else if (algo_selected === "Merge Sort")
             await mergeSort(0, size - 1);
-        else if (algo_selected === "Quicksort")
+        else if (algo_selected === "Quick Sort")
             await quicksort(0, size - 1);
-        else if (algo_selected === "Heapsort")
+        else if (algo_selected === "Heap Sort")
             await heapsort();
         else {
             // No algorithm selected -> show popup
